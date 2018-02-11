4911a5b8284b59700773b0bd2113c230f0ef6bc8
Anwesh43 <anweshthecool0@gmail.com>
2018-02-12 01:21:56 +0530

    adding TwoCircleFill class

--- twocirclefillview/src/main/java/ui/anwesome/com/twocirclefillview/TwoCircleFillView.kt
+++ twocirclefillview/src/main/java/ui/anwesome/com/twocirclefillview/TwoCircleFillView.kt
@@ -20,4 +20,29 @@ class TwoCircleFillView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class TwoCircleFill(var x:Float,var y:Float,var size:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.color = Color.parseColor("#f44336")
+            paint.strokeWidth = size/20
+            paint.strokeCap = Paint.Cap.ROUND
+            canvas.drawClippedCircle(x-size/2,y,size/6,1f,paint)
+            canvas.drawLine(x-size/3+2*size/3,y,x-size/3+2*size/3,y,paint)
+            canvas.drawClippedCircle(x+size/2,y,size/6,1f,paint)
+        }
+        fun update(stopcb:(Float) -> Unit) {
+
+        }
+        fun startUpdating(startcb:() -> Unit) {
+
+        }
+    }
+}
+fun Canvas.drawClippedCircle(x:Float,y:Float,r:Float,scale:Float,paint:Paint) {
+    save()
+    translate(x,y)
+    val path = Path()
+    path.addCircle(0f,0f,r,Path.Direction.CW)
+    clipPath(path)
+    drawRect(RectF(-r, r - 2*r*scale, r, r),paint)
+    restore()
 }
