c785cd6b63a08fc3c3bc3f2176f5ac0944e4b152
Anwesh43 <anweshthecool0@gmail.com>
2018-02-12 01:00:29 +0530

    adding TwoCircleFillView class

--- twocirclefillview/src/main/java/ui/anwesome/com/twocirclefillview/TwoCircleFillView.kt
+++ twocirclefillview/src/main/java/ui/anwesome/com/twocirclefillview/TwoCircleFillView.kt
@@ -3,3 +3,21 @@ package ui.anwesome.com.twocirclefillview
 /**
  * Created by anweshmishra on 12/02/18.
  */
+import android.graphics.*
+import android.content.*
+import android.view.*
+
+class TwoCircleFillView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun draw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
