b05191439aed3f87400ccb371d9b52687a88082d
Anwesh43 <anweshthecool0@gmail.com>
2018-02-12 02:07:29 +0530

    using view in activity

--- app/src/main/java/ui/anwesome/com/kotlintwocirclefillview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlintwocirclefillview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlintwocirclefillview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.twocirclefillview.TwoCircleFillView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TwoCircleFillView.create(this)
     }
 }
