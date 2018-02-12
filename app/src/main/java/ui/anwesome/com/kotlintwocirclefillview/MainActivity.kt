0e9c7cc79694b64a0ead1fc0c540ef53883eb87f
Anwesh43 <anweshthecool0@gmail.com>
2018-02-12 07:54:36 +0530

    adding fullscreen support for Activity

--- app/src/main/java/ui/anwesome/com/kotlintwocirclefillview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlintwocirclefillview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlintwocirclefillview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Toast
 import ui.anwesome.com.twocirclefillview.TwoCircleFillView
 
@@ -13,5 +14,11 @@ class MainActivity : AppCompatActivity() {
         view.addCircleFillListener {
             Toast.makeText(this,"$it filled",Toast.LENGTH_SHORT).show()
         }
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
