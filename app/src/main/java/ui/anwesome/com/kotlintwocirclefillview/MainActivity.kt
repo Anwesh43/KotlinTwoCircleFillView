f6d5604099fe21185ff8e8597101e15d2103c2e1
Anwesh43 <anweshthecool0@gmail.com>
2018-02-12 07:50:55 +0530

    using CircleFillListener in Activity

--- app/src/main/java/ui/anwesome/com/kotlintwocirclefillview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlintwocirclefillview/MainActivity.kt
@@ -2,12 +2,16 @@ package ui.anwesome.com.kotlintwocirclefillview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.twocirclefillview.TwoCircleFillView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        TwoCircleFillView.create(this)
+        val view = TwoCircleFillView.create(this)
+        view.addCircleFillListener {
+            Toast.makeText(this,"$it filled",Toast.LENGTH_SHORT).show()
+        }
     }
 }
